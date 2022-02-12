7df22de3ffd43e96e588f0fc2a13f0d0e01985fa
Ivan <chakin.2001@mail.ru>
2022-02-12 10:35:05 +0300

    vue-cli init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <Header/>
+    <div class="wrapper-content">
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+export default {
+  components: {
+    Header,
+    Footer
+  },
+}
+</script>
+
+<style>
+</style>
