6da96f691c971c1893a2f37f89400b14e0969b12
Ivan <chakin.2001@mail.ru>
2022-02-13 21:48:26 +0300

    add Form & List

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,36 @@
 <template>
-  <p>Hello home!</p>
-  <p>Hello home2</p>
+  <Form @onSubmit="handleSubmit" />
+  <List @onRemove="handleRemove" :items="notes" />
 </template>
 
 <script>
+import Form from '@/components/Notes/Form'
+import List from "@/components/Notes/List";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    Form,
+    List
+  },
+  data () {
+    return {
+      notes: ['task 1', 'task 2', 'task 3']
+    }
+  },
+  methods: {
+    handleSubmit(note) {
+      this.notes.push(note)
+      console.log(note)
+    },
+    handleRemove(index) {
+      this.notes.splice(index, 1)
+    }
+  }
 }
 </script>
 
 <style scoped>
 
 </style>
-\
+
