3270c5c44056b606b4dc55d62a46a0649c2a43f2
Ivan <chakin.2001@mail.ru>
2022-02-20 16:45:54 +0300

    + vuex & + store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import routers from './routes'
+import {store} from './store'
 
 import '../assets/scss/main.scss'
 
 const app = createApp(App);
 
 app.use(routers)
+app.use(store)
 
 app.mount("#app");
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,65 +1,7 @@
 <template>
-  <Form @onSubmit="handleSubmit" />
-  <List @onRemove="handleRemove" :items="notes" />
+  <p>Vuex</p>
 </template>
 
 <script>
-import Form from '@/components/Notes/Form'
-import List from "@/components/Notes/List";
 
-export default {
-  name: "home",
-  components: {
-    Form,
-    List
-  },
-  data () {
-    return {
-      notes: [
-        {
-          title: 'Learn Vue 3',
-          tags: ['work']
-        },
-        {
-          title: 'Finish course',
-          tags: ['work', 'home']
-        }
-      ]
-    }
-  },
-  mounted() {
-    this.getNotes()
-  },
-  methods: {
-    handleSubmit(title) {
-      const note = {
-        title: title,
-        tags: []
-      }
-      this.notes.push(note)
-    },
-    handleRemove(index) {
-      this.notes.splice(index, 1)
-    },
-    getNotes() {
-      const localNotes = localStorage.getItem("notes");
-      if (localNotes) {
-        this.notes = JSON.parse(localNotes)
-      }
-    }
-  },
-  watch: {
-    notes: {
-      handler(updateList) {
-        localStorage.setItem('notes', JSON.stringify(updateList))
-      },
-      deep: true
-    }
-  }
-}
 </script>
-
-<style scoped>
-
-</style>
-
